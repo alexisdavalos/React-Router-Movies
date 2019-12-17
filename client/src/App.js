89bf03bf7ced771dfd036573990414fde997186c
Alexis <alexisdavalos.tech@gmail.com>
2019-12-17 18:28:11 -0500

    Refactored to DRY Code: Movie.js and MovieList.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+import axios from 'axios';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -8,11 +11,36 @@ const App = () => {
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
+  //get movies from API and set to State
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getMovies();
+  }, []);
 
   return (
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      {/* Delcare Routes */}
+      <Switch>
+        <Route path="/movies/:movieNum">
+          <Movie movies={movies}/>
+        </Route>
+        <Route exact path="/">
+          <MovieList movies={movies}/>
+        </Route>
+      </Switch>
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,52 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import MovieCard from './MovieCard';
 
-const MovieList = props => {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
+const MovieList = ({movies}) => {
+  // Commented out Old API Call
+  // const [movies, setMovies] = useState([])
+  // useEffect(() => {
+  //   const getMovies = () => {
+  //     axios
+  //       .get('http://localhost:5000/api/movies')
+  //       .then(response => {
+  //         setMovies(response.data);
+  //       })
+  //       .catch(error => {
+  //         console.error('Server Error', error);
+  //       });
+  //   }
     
-    getMovies();
-  }, []);
-  
+  //   getMovies();
+  // }, []);
+  console.log('This is the Movies Object from: MovieList.Js', movies)
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie}/>
       ))}
     </div>
   );
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+// function MovieDetails({ movie }) {
+//   const { title, director, metascore, stars, id } = movie;
+//   return (
+//     <Link to={`/movies/${id}`}>
+//     <div className="movie-card">
+//       <h2>{title}</h2>
+//       <div className="movie-director">
+//         Director: <em>{director}</em>
+//       </div>
+//       <div className="movie-metascore">
+//         Metascore: <strong>{metascore}</strong>
+//       </div>
+//       <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
+//       {stars.map(star => (
+//         <div key={star} className="movie-star">
+//           {star}
+//         </div>
+//       ))}
+//     </div>
+//     </Link>
+//   );
+// }
 
 export default MovieList;
